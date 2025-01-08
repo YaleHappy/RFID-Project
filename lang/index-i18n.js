63f9d94ece6af8c9ace62efc93beb09ef6056b57
YaleHappy <91077787+YaleHappy@users.noreply.github.com>
2025-01-09 02:11:31 +0800

    Add files via upload

--- lang/index-i18n.js
+++ lang/index-i18n.js
@@ -19,6 +19,14 @@ export const translations = {
         indexfoter2: '聯絡我們：41241211@nfu.edu.tw ｜　41241215@nfu.edu.tw',
         indexcarousel1:'麥香紅茶十塊錢 歡迎購買！',
         indexcarousel2:'科學麵也十塊錢 歡迎購買！',
+        userGuidelineTitle: '使用者規範與操作說明',
+        userGuidelineHeading: '使用者規範',
+        userGuidelinePoint1: '如果有其他需求，請知會我們一聲，感謝',
+        userOperationHeading: '使用者操作說明',
+        userOperationInstruction: '系統中有隱藏的聚焦框（Focus Box）您可以直接在背景輸入RFID並按下 Enter 鍵來提交操作，而不需要點擊特定按鈕。 例如，像是在儲值頁面，會有框直接輸入會顯示ID，在其他頁面也是同理，只是隱藏起來了而已。',
+        userAdditionalNote: '安比薩遊樂園保留隨時修正、暫停、終止或解釋本規範及使用者操作之最終權利',
+        closeButton: '關閉',
+        checkuserrulemodal:'查看使用者規範與操作說明',
     },
     'en': {
         title: 'Ambisa Amusement Park',
@@ -40,6 +48,14 @@ export const translations = {
         indexfoter2: 'Contact us: 41241211@nfu.edu.tw ｜ 41241215@nfu.edu.tw',
         indexcarousel1:'black tea is 10NT welcome to buy!',
         indexcarousel2:'science noodles is 10NT welcome to buy!',
+        userGuidelineTitle: 'User Guidelines and Instructions',
+        userGuidelineHeading: 'User Guidelines',
+        userGuidelinePoint1: 'If you have any other requirements, please let us know. Thank you.',
+        userOperationHeading: 'User Operation Instructions',
+        userOperationInstruction: 'The system has hidden focus boxes. You can directly input RFID data in the background and press the Enter key to submit, without needing to click a specific button. For example, on the top-up page, there will be a box to input directly showing the ID; similarly on other pages, it is hidden but functional.',
+        userAdditionalNote: 'If Ambisa Amusement Park reserves the final right to amend, suspend, terminate, or interpret these guidelines and user operations at any time. are other operations you need reminders for, please refer to these instructions for detailed information.',
+        closeButton: 'Close',
+        checkuserrulemodal:'View user specifications and operating instructions',
     }
 };
 
